6b015bee5621c743fda5ada38ee4e1e82d15b2d1
jone601214 <32056035+jone601214@users.noreply.github.com>
2018-06-23 23:14:22 +0800

    Add files via upload

---
+++ MAINFRAME2.java
@@ -0,0 +1,170 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+public class MAINFRAME2 extends JFrame{
+    private JButton jbt = new JButton("START");
+
+    private JButton jbtS = new JButton("STOP");
+
+    private JButton jbt2 = new JButton("RESET");
+
+    private JButton jbt1 = new JButton("EXIT");
+
+    private JLabel jlb1 = new JLabel();
+
+    private JLabel jb1 = new JLabel( );//圖片
+
+    private JLabel jlb = new JLabel();
+
+    private JLabel jlb2 = new JLabel();
+
+    private Random rand = new Random();
+
+    private ImageIcon img1 = new ImageIcon("3.png");
+
+    private Timer t1;
+
+    private Container cp;
+
+    private  int count =0;
+
+    private String userName;
+
+    public MAINFRAME2(String name){
+
+        userName = name;
+
+        init();
+    }
+    private void init(){
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        this.setResizable(false);
+
+        this.setBounds(0,0,800,600);
+
+        cp = this.getContentPane();
+
+        jlb.setText(userName);
+
+        this.add(jbt);
+
+        this.add(jbtS);
+
+        this.add(jbt2);
+
+        this.add(jbt1);
+
+        this.add(jb1);
+
+        this.add(jlb);
+
+        this.add(jlb1);
+        this.add(jlb2);
+
+        jbt.setBounds(0,0,150,150);
+
+        jbt.setFont(new Font(null,Font.BOLD,25));
+
+        jbtS.setBounds(0,150,150,150);
+
+        jbtS.setFont(new Font(null,Font.BOLD,25));
+
+        jbt2.setBounds(0,280,150,150);
+
+        jbt2.setFont(new Font(null,Font.BOLD,25));
+
+        jbt1.setBounds(0,420,150,150);
+
+        jbt1.setFont(new Font(null,Font.BOLD,25));
+
+        jb1.setBounds(400,200,113,113);
+
+        jb1.setIcon(img1);
+
+        jlb.setBounds(450,20,100,50);
+
+        jlb.setFont(new Font(null,Font.BOLD,25));
+
+        jlb1.setBounds(200,20,100,50);
+
+        jlb1.setFont(new Font(null,Font.BOLD,25));
+
+
+
+
+
+        t1 = new Timer(500, new AbstractAction() {
+
+            @Override
+
+            public void actionPerformed(ActionEvent e) {
+                count = (int) count + 1 ;
+                jlb1.setText("數值:"+Integer.toString(count));
+            }
+
+        });
+
+        jbt.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+
+                t1.start();
+                int[] num=new int[100];
+
+                int x=1,cnt=0,flag=0;
+
+                while (cnt < 1){
+
+                    x=rand.nextInt(100)+1;
+
+                    for (int i=0; i<cnt; i++)
+
+                        if (x==num[i]){
+
+                            flag++;
+
+                            break;
+                        }
+                    if (flag==0 ){
+
+                        num[cnt]=x;
+
+                        cnt++;
+                    }
+                    else
+                        flag=0;
+                }
+                for (int i=0; i<1; i++) {
+                    System.out.println(num[i]);
+                }
+            }
+        });
+        jbtS.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                t1.stop();
+                JOptionPane.showMessageDialog(null,"遊戲結束");
+            }
+        });
+        jbt1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        jbt2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                t1.stop();
+                jlb1.setText("數值:"+Integer.toString(count=0));
+            }
+        });
+    }
+}
